1f64f80e8d6a6f70378169e5982577dbf89c9db0
nikguscode <nikguscode@gmail.com>
2023-09-28 21:39:37 +0500

    Fix code

--- src/main/java/com/nikguscode/TaskTimer/controller/MenuController.java
+++ src/main/java/com/nikguscode/TaskTimer/controller/MenuController.java
@@ -1,18 +1,21 @@
 package com.nikguscode.TaskTimer.controller;
 
+import com.nikguscode.TaskTimer.controller.commands.HelloCommand;
+import com.nikguscode.TaskTimer.model.service.telegramConnection.BotConnection;
 import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Controller
 public class MenuController {
 
+    private BotConnection botConnection;
+
+    public MenuController(BotConnection botConnection) {
+        this.botConnection = botConnection;
+    }
+
     public void mainRedirecting(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-
-            String messageText = update.getMessage().getText();
-
-
-
 
         }
     }
--- src/main/java/com/nikguscode/TaskTimer/controller/commands/HelloCommand.java
+++ src/main/java/com/nikguscode/TaskTimer/controller/commands/HelloCommand.java
@@ -3,7 +3,6 @@ package com.nikguscode.TaskTimer.controller.commands;
 public class HelloCommand {
 
     public void execute() {
-        System.out.println("Hello world");
     }
 
 }
