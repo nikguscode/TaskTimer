53de7eeb3739fdb948ba364efe0160215662a7fa
nikguscode <nikguscode@gmail.com>
2023-11-10 22:46:49 +0500

    Создан класс, который содержит в себе команды бота и прочие текстовые константы.

---
+++ src/main/java/com/nikguscode/TaskTimer/model/PhraseConstants.java
@@ -0,0 +1,13 @@
+package com.nikguscode.TaskTimer.model;
+
+import com.nikguscode.TaskTimer.view.EmojiConstants;
+
+public class PhraseConstants {
+
+    public static final String SUCCESSFULLY = "Успешно";
+    public static final String BACK_TO_MENU = EmojiConstants.BACK_TO_MENU_ICON + " Вернуться в главное меню";
+    public static final String RESTART_BOT_CMD = "/start";
+    public static final String TYPE_MANAGEMENT = EmojiConstants.FOLDER_ICON + " Управление типами";
+    public static final String CATEGORY_LIST = EmojiConstants.LIST_ICON + " Список категорий";
+
+}
