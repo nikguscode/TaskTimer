8ce24a2e7ff7d21298dd8ecda0b3f796b073c1ab
nikguscode <nikguscode@gmail.com>
2023-11-10 22:45:19 +0500

    Аннотации сканирования для Spring Data JPA

--- src/main/java/com/nikguscode/TaskTimer/TaskTimerApplication.java
+++ src/main/java/com/nikguscode/TaskTimer/TaskTimerApplication.java
@@ -2,9 +2,13 @@ package com.nikguscode.TaskTimer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.nikguscode.TaskTimer.model.entity")
+@EnableJpaRepositories(basePackages = "com.nikguscode.TaskTimer.model.repository")
 public class TaskTimerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTimerApplication.class, args);
